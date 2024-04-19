62a97d4c6372a41b0416b5535bc8e73422b381c2
Dmitriy1197 <dmitriy.gavrilyuk.1999@gmail.com>
2024-04-19 14:45:27 +0300

    Created base project, removed storyboard

---
+++ FooddevApp/FooddevApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FooddevApp
+//
+//  Created by Dima on 19.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green    }
+
+
+}
+
