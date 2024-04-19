3430ba0c1bbf3072e24339d5cec97a07addb6046
Dmitriy1197 <dmitriy.gavrilyuk.1999@gmail.com>
2024-04-19 15:13:18 +0300

    Added new fonts

--- FooddevApp/FooddevApp/ViewController.swift
+++ FooddevApp/FooddevApp/ViewController.swift
@@ -11,21 +11,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
-        
+        view.backgroundColor = .red
         let label = UILabel()
-        label.text = "Hello World!"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
         view.addSubview(label)
+        label.text = "Hello world!"
+        label.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            label.topAnchor.constraint(equalTo: view.topAnchor,constant: 200),
-            label.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
+            label.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0),
             label.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 20)
         ])
-        label.font = .systemFont(ofSize: 40, weight: UIFont.Weight(rawValue: 20))
-        
-        
+        label.font = .Roboto.blackItalic.size(of: 60)
+       // label.textColor = AppColors.accentOrange
     }
     
 
